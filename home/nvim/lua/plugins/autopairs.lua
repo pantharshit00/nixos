0ae57d8adea0fb4fa1a3d22efa795999fb0d110e
Harshit Pant <pantharshit00+github@gmail.com>
2021-05-21 18:47:22 +0530

    remove unnecessay autopair code

--- home/nvim/lua/plugins/autopairs.lua
+++ home/nvim/lua/plugins/autopairs.lua
@@ -2,41 +2,13 @@ require('nvim-autopairs').setup()
 local npairs = require('nvim-autopairs')
 
 local function imap(lhs, rhs, opts)
-    local options = {noremap = false}
-    if opts then options = vim.tbl_extend('force', options, opts) end
-    vim.api.nvim_set_keymap('i', lhs, rhs, options)
+    vim.api.nvim_set_keymap('i', lhs, rhs, opts)
 end
 
 _G.MUtils = {}
 
--- TEST
 vim.g.completion_confirm_key = ""
 
-MUtils.tab = function()
-    if vim.fn.pumvisible() ~= 0 then
-        return npairs.esc("<C-n>")
-    else
-        if vim.fn["vsnip#available"](1) ~= 0 then
-            vim.fn.feedkeys(string.format('%c%c%c(vsnip-expand-or-jump)', 0x80, 253, 83))
-            return npairs.esc("")
-        else
-            return npairs.esc("<Tab>")
-        end
-    end
-end
-
-MUtils.s_tab = function()
-    if vim.fn.pumvisible() ~= 0 then
-        return npairs.esc("<C-p>")
-    else
-        if vim.fn["vsnip#jumpable"](-1) ~= 0 then
-            vim.fn.feedkeys(string.format('%c%c%c(vsnip-jump-prev)', 0x80, 253, 83))
-            return npairs.esc("")
-        else
-            return npairs.esc("<C-h>")
-        end
-    end
-end
 
 MUtils.completion_confirm=function()
   if vim.fn.pumvisible() ~= 0  then
